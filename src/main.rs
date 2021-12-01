c429da9e21d7b942d648266b4c9c832e9414df2c
bec <aoc@binarytrove.com>
2021-12-01 21:25:39 +0100

    p1_2

--- src/main.rs
+++ src/main.rs
@@ -21,4 +21,13 @@ mod tests {
             .map(solve::p1_1);
         assert_eq!(depth_increases, Ok(7))
     }
+
+    #[test]
+    fn day1_ex2() {
+        let depth_increases = fs::read_to_string("examples/1-1")
+            .map_err(to_module_error)
+            .and_then(parse::p1_1)
+            .map(solve::p1_2);
+        assert_eq!(depth_increases, Ok(5))
+    }
 }
--- src/solve.rs
+++ src/solve.rs
@@ -1,3 +1,11 @@
+pub fn p1_1_imperative(vec: Vec<usize>) -> usize {
+    let mut acc = 0;
+    for i in 1..vec.len() {
+        if vec[i] > vec[i-1] { acc +=1; }
+    };
+    acc
+}
+
 pub fn p1_1(vec: Vec<usize>) -> usize {
    let (_, sum) = vec.iter()
        .fold((None, 0), |state, item| match state {
@@ -7,4 +15,27 @@ pub fn p1_1(vec: Vec<usize>) -> usize {
                 (Some(item), if item > pred {acc+1} else {acc})
        });
    sum
+}
+
+pub fn _p1_2_imperative(vec: Vec<usize>) -> usize {
+    let mut acc = 0;
+    for i in 3..vec.len() {
+        let previous_window: usize = vec[i-3..i-1].iter().sum();
+        let current_window: usize = vec[i-2..i].iter().sum();
+        if previous_window < current_window {
+            acc += 1;
+        }
+    };
+    acc
+}
+
+pub fn p1_2(vec: Vec<usize>) -> usize {
+    let digits_1 = vec[0..vec.len()-2].iter();
+    let digits_2 = vec[1..vec.len()-1].iter();
+    let digits_3 = vec[2..vec.len()].iter();
+
+    let windows = digits_1.zip(digits_2).zip(digits_3)
+        .map(|((d_1, d_2), d_3)| d_1 + d_2 + d_3);
+
+    p1_1(windows.collect())
 }
